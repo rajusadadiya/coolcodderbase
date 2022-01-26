b1d86c4e929114e0f4975fbeb8ee41511415173d
raju_icreon <raju.sadadiya@icreon.com>
2022-01-26 18:36:53 +0530

    Initial Commit in Magento 2 Base module

---
+++ registration.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Cool Codders
+ *
+ * NOTICE OF LICENSE
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to newer
+ * version in the future.
+ *
+ * @category CoolCodders Extension
+ * @package  CoolCodders_Base
+ * @author   Cool Devs <cooldev@gmail.com>
+ * @license  OSL 3.0
+ * @link     http://www.coolcodders.com
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'CoolCodders_Base',
+    __DIR__
+);
